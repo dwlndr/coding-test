ddd90bdf93bf0654b0750b3982cf010be0827939
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-09-03 19:45:36 +0700

    20 menit

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let arr = []
-function addArray(arr) {
+function addArray() {
     let number = document.getElementById('number').value;
 
     if (number !== '') {
+        arr.push(Number(number));
+
+
+    }
+}
+
+function jumlah() {
+    for (let i = 0; i < array.length; i++) {
+        let total += array[i];
         
     }
 }
