4f7b1297a3435e7f2fdf7964ea3b9a9b37cbd214
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-09-03 19:56:26 +0700

    30 menit

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let arr = []
-function addArray() {
-    let number = document.getElementById('number').value;
+// let arr = [];
+// function addArray() {
+//   let number = document.getElementById("number").value;
 
-    if (number !== '') {
-        arr.push(Number(number));
-    }
-    jumlah()
-}
+//   if (number !== "") {
+//     arr.push(Number(number));
+//   }
+//   jumlah();
+// }
 
-function jumlah() {
+// function jumlah() {
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i];
+//   }
+//   return total;
+//   document.getElementById("total").textContent = total;
+// }
+
+function jumlaharray(array){
+    let total = 0
     for (let i = 0; i < array.length; i++) {
-        total += array[i];
+        total = total + array[i];
     }
     return total;
-    document.getElementById('total').textContent = total;
 }
+
+console.log(jumlaharray([1,2,3,4,5]));
